5abe3f3402de3e92eaee7e66f9e701105abdd8d5
Meenakshi <meenakshijoshi2597@gmail.com>
2022-11-30 16:48:36 +0530

    feat: add user-agent endpoint

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const http = require('http')
-
+var agent = new http.Agent({});
 const PORT = process.env.PORT || 3000
-
+const options = {
+  host: 'google.com',
+  port: 80,
+  path: '/',
+  method: 'GET',
+  agent: agent,
+};
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
 
   res.end()
 })
+// Requesting details via http server module
+const req = http.request(options, (res) => {
+  console.log("Headers: ", res.headers);
+});
 
 function respondHello (req, res) {
   res.end(JSON.stringify({ msg: 'hello' }))
